f1186801007bfcaaba55692ac6403ed6d332c7ce
Catherine <ducharme.catherine@gmail.com>
2015-09-29 17:32:01 +0000

    Outputted, the ids, emails and account names using the 'billion queries' method

---
+++ joining-data.js
@@ -0,0 +1,38 @@
+var mysql = require('mysql');
+var Promise = require('bluebird');
+var colors = require('colors');
+Promise.promisifyAll(mysql);
+Promise.promisifyAll(require("mysql/lib/Connection").prototype);
+Promise.promisifyAll(require("mysql/lib/Pool").prototype);
+
+var connection = mysql.createConnection({
+  host     : process.env.IP,
+  user     : process.env.C9_USER,
+  password : '',
+  database : 'addressbook'
+});
+
+connection.queryAsync("SELECT id, (concat('#', id, ':')) AS 'ID', email FROM Account LIMIT 10;").spread(
+    function(results) {
+    	return results;
+    }    
+).map(
+    function(accounts) {
+        connection.queryAsync("SELECT name AS 'Name' FROM AddressBook WHERE accountId =" + accounts['id'] + ";").spread(
+            function(results) {
+                console.log(accounts['ID'].bold, "\t", accounts["email"].underline);
+                return results;
+            }
+        ).map(
+           function(names) {
+               console.log ("\t", names["Name"].yellow);
+           }     
+        );
+        
+    }    
+).finally(
+    function() {
+        connection.end();
+    }
+);
+
