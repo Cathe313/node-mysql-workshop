43a6f06c795f4522ed780ecb906738a28af15bf3
Catherine <ducharme.catherine@gmail.com>
2015-09-30 23:05:42 +0000

    Aggregated the Address Books by account ID

--- joining-data-3.js
+++ joining-data-3.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var Promise = require('bluebird');
+var colors = require('colors');
 Promise.promisifyAll(mysql);
 Promise.promisifyAll(require("mysql/lib/Connection").prototype);
 Promise.promisifyAll(require("mysql/lib/Pool").prototype);
@@ -11,11 +12,25 @@ var connection = mysql.createConnection({
   database : 'addressbook'
 });
 
-connection.queryAsync("SELECT Account.id, Account.email, Account.password, Account.createdOn, Account.modifiedOn, AddressBook.id AS addressBookId, AddressBook.accountId, AddressBook.name, AddressBook.createdOn AS AddressBookCreationDate, AddressBook.modifiedOn AS AddressBookModifiedDate FROM Account JOIN AddressBook ON AddressBook.accountId = Account.id;").spread(
+connection.queryAsync("SELECT (concat('#', Account.id, ':')) AS 'ID', Account.email, Account.password,\
+                Account.createdOn, Account.modifiedOn, AddressBook.id AS AddressBookId, AddressBook.name, \
+                AddressBook.createdOn AS AddressBookCreatedDate, AddressBook.modifiedOn AS AddressBookModifiedDate \
+                FROM Account \
+                JOIN AddressBook ON AddressBook.accountId = Account.id \
+                ORDER BY Account.id;").spread(
     function(results) {
-        console.log(results);
-        return results;
-    }    
+        var ids = [];
+        for (var i = 0; i < results.length; i++) {
+            if (ids.indexOf(results[i]['ID']) === -1) {
+                ids.push(results[i]['ID']);
+                console.log(results[i]['ID'].bold.yellow);
+                console.log("\t" + results[i]['name'].rainbow);
+            }
+            else {
+                console.log("\t" + results[i]['name'].rainbow);
+            }
+        }
+    }
 ).finally(
     function() {
         connection.end();
